fea6dcc24b788dd02c0d98762e0ae850199ddb43
Jordan Green <thegreenjordan@gmail.com>
2022-01-29 13:44:54 -0500

    Double tap shift for caps lock

--- keyboards/gmmk/pro/ansi/keymaps/jgreen/keymap.c
+++ keyboards/gmmk/pro/ansi/keymaps/jgreen/keymap.c
@@ -50,8 +50,10 @@ typedef struct {
     td_state_t state;
 } td_tap_t;
 
+//Tap dance keys
 enum {
-    JP_CAPS, // Our custom tap dance key; add any other tap dance keys to this enum 
+    TD_JP_CAPS,  //Tap caps for JP/ENG, hold for layer 1
+    TD_SFT_CAPS, //Double tap shift for caps lock
 };
 
 // Function associated with all tap dances
@@ -74,8 +76,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  C_A_D,            KC_MUTE,
         KC_GRV,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_HOME,
         KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          DSK_L,
-        TD(JP_CAPS),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           DSK_R,
-        KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
+        TD(TD_JP_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           DSK_R,
+        TD(TD_SFT_CAPS),KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
         KC_LCTL,        KC_LGUI, KC_LALT,                             KC_SPC,                             KC_DEL, MO(1),   SCRNSHT, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
@@ -404,5 +406,6 @@ void ql_reset(qk_tap_dance_state_t *state, void *user_data) {
 
 // Associate our tap dance key with its functionality
 qk_tap_dance_action_t tap_dance_actions[] = {
-    [JP_CAPS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, ql_finished, ql_reset)
+    [TD_JP_CAPS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, ql_finished, ql_reset),
+    [TD_SFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
 };
